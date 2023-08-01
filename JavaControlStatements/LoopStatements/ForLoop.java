de1c33d3a02556c1675027d717bcacc86fa094e4
Varsha Kaveri <varsha.kaveri24@gmail.com>
2023-08-01 15:10:19 +0530

    loop statements programs add on

---
+++ JavaControlStatements/LoopStatements/ForLoop.java
@@ -0,0 +1,10 @@
+// Java For Loop Program 
+public class ForLoop{
+    public static void main(String args[]){
+        for(int i = 1;i<=15;i++){
+            if(i % 2 == 1){
+                System.out.println(i);
+            }
+        }
+    }
+}
