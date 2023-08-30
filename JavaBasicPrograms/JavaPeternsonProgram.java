8f7d33ba86a02850d59eaa21a7b54e0c2012bd81
Varsha Kaveri <140880760+varshakaveri@users.noreply.github.com>
2023-08-30 16:42:21 +0530

    type conversion

--- JavaBasicPrograms/JavaPeternsonProgram.java
+++ JavaBasicPrograms/JavaPeternsonProgram.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class JavaPetersonProgram{
     public static void main(String args[]){
-        
+        Scanner sc  = new Scanner(System.in);
+        int number = sc.nextInt();
+        int fact = 1;
+        for(int i =1;i<number;)
     }
 }
---
+++ JavaBasicPrograms/JavaTypeConversion.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class JavaTypeConversion{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int number = 500;
+        String strNumber = String.valueOf(number);
+        System.out.println(String);
+        System.out.println(strNumber.getClass());
+
+    }
+}
